aab3028dcc8cc421880b28ae5130afd5bdf7096a
AbrarShahriar <AbrarShahriar123@gmail.com>
2022-10-10 18:36:25 +0600

    first commit

---
+++ android/app/src/main/java/com/abrarshahriar/lovealarm/MainActivity.java
@@ -0,0 +1,5 @@
+package com.abrarshahriar.lovealarm;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ android/app/src/main/java/com/abrarshahriar/lovealarm/NotificationServiceExtension.java
@@ -0,0 +1,26 @@
+package com.abrarshahriar.lovealarm;
+  
+import android.content.Context;
+import android.util.Log;
+import org.json.JSONObject;
+
+import com.onesignal.OSNotification;
+import com.onesignal.OSMutableNotification;
+import com.onesignal.OSNotificationReceivedEvent;
+import com.onesignal.OneSignal.OSRemoteNotificationReceivedHandler;
+
+@SuppressWarnings("unused")
+public class NotificationServiceExtension implements OSRemoteNotificationReceivedHandler {
+
+    @Override
+    public void remoteNotificationReceived(Context context, OSNotificationReceivedEvent notificationReceivedEvent) {
+        OSNotification notification = notificationReceivedEvent.getNotification();
+
+        JSONObject data = notification.getAdditionalData();
+        Log.i("OneSignalExample", "Received Notification Data: " + data);
+
+        // If complete isn't call within a time period of 25 seconds, OneSignal internal logic will show the original notification
+        // To omit displaying a notification, pass `null` to complete()
+        notificationReceivedEvent.complete(notification);
+    }
+}
